325b0886aa176b8e3efe3e1b7fa22092e15387d8
t.rosko <t.rosko@digibit.pl>
2024-04-08 21:18:56 +0200

    add recaptcha

--- src/components/Contact/Email.js
+++ src/components/Contact/Email.js
@@ -3,17 +3,25 @@ import React, {useState} from 'react';
 import emailjs from '@emailjs/browser';
 import Container from "react-bootstrap/Container";
 import {Col, Row} from "react-bootstrap";
+import ReCAPTCHA from 'react-google-recaptcha';
+
+const RECAPTCHA_KEY = process.env.React_App_RECAPTCHA_KEY;
 
 const Email = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [reCAPTCHAValue, setReCAPTCHAValue] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Your EmailJJ Date
+    if (!reCAPTCHAValue) {
+      alert('Please verify that you are not a robot.');
+      return;
+    }
 
+    // Your EmailJS Data
     const serviceId = process.env.React_App_SERVICE_ID;
     const templateId = process.env.React_App_TEMPLATE_ID;
     const publicKey = process.env.React_App_PUBLIC_KEY;
@@ -26,63 +34,68 @@ const Email = () => {
       message: message,
     };
 
-    //Send Email
+    // Send Email
     emailjs.send(serviceId, templateId, templateParams, publicKey)
-      .then((response) => {
-        // console.log('Email sent successfully!', response);
-        alert("Email sent successfully!")
-        setName('');
-        setEmail('');
-        setMessage('');
-      })
-      .catch((error) => {
-        // console.error('Error sending email:', error);
-        alert("Error sending email")
-      });
-  }
+        .then((response) => {
+          alert("Email sent successfully!");
+          setName('');
+          setEmail('');
+          setMessage('');
+          setReCAPTCHAValue('');
+        })
+        .catch((error) => {
+          alert("Error sending email");
+        });
+  };
+
+  const handleReCAPTCHAChange = (value) => {
+    setReCAPTCHAValue(value);
+  };
+
   return (
-    <section className={"c-contact"}>
-      <Container fluid>
-        <Container>
-          <Row className={"c-contact__box"}>
-            <Col xs={12} md={8}>
-              <h2>Kontakt</h2>
-              <form onSubmit={handleSubmit} className='emailForms'>
-                <label>
-                  Twoje imię:
-                  <input
-                    type="text"
-                    placeholder="Twoje imię"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </label>
-                <label>
-                  Twój Email:
-                  <input
-                    type="email"
-                    placeholder="Twój adres Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </label>
-                <label>
-                  Tekst wiadomości:
-                  <textarea
-                    cols={40}
-                    rows={12}
-                    value={message}
-                    placeholder="Tutaj wpisz treść"
-                    onChange={(e) => setMessage(e.target.value)}>
+      <section className={"c-contact"}>
+        <Container fluid>
+          <Container>
+            <Row className={"c-contact__box"}>
+              <Col xs={12} md={8}>
+                <h2>Kontakt</h2>
+                <form onSubmit={handleSubmit} className='emailForms'>
+                  <label>
+                    Twoje imię:
+                    <input
+                        type="text"
+                        placeholder="Twoje imię"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                  </label>
+                  <label>
+                    Twój Email:
+                    <input
+                        type="email"
+                        placeholder="Twój adres Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </label>
+                  <label>
+                    Tekst wiadomości:
+                    <textarea
+                        cols={40}
+                        rows={12}
+                        value={message}
+                        placeholder="Tutaj wpisz treść"
+                        onChange={(e) => setMessage(e.target.value)}>
                    </textarea>
-                </label>
-                <button className={"btn-send"} type="submit">Wyślij wiadomość</button>
-              </form>
-            </Col>
-          </Row>
+                  </label>
+                  <ReCAPTCHA sitekey={RECAPTCHA_KEY} onChange={handleReCAPTCHAChange} />
+                  <button className={"btn-send"} type="submit">Wyślij wiadomość</button>
+                </form>
+              </Col>
+            </Row>
+          </Container>
         </Container>
-      </Container>
-    </section>
+      </section>
   )
 }
 

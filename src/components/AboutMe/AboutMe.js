a93ae2f7c2bb8c3ec5690722d6cb5f1eba5c3c75
t.rosko <t.rosko@digibit.pl>
2024-05-03 07:58:26 +0200

    feat(AboutMe) refactor content insertion into constants and reverse const

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,6 +5,20 @@ import LogoBlack from "../../assets/img/svg/logo-black.svg";
 import Phone from "../../assets/img/svg/phone.svg";
 import Message from "../../assets/img/svg/message.svg";
 
+const firstName = 'Magdalena';
+const lastName = "Rośko";
+const mobileNumber = "+48 667 264 375";
+const email = 'magdalena.chuchmacz@wp.pl'
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const firstNameReverse = reverseString(firstName)
+const lastNameReverse = reverseString(lastName)
+const mobileNumberReverse = reverseString(mobileNumber)
+const emailReverse = reverseString(email)
+
 function SectionDate() {
   return (
     <section className={"section-date"}>
@@ -27,9 +41,9 @@ function SectionDate() {
             </Col>
             <Col md={4} className={"box-contact"}>
               <div className="title" id={"contact"}>Kontakt</div>
-              <a href="/" className={"link-footer"}><img src={LogoBlack} className={"ic-logo-black"} width={24} height={15} alt="Logo" /><p>okśoR aneladgaM</p></a>
-              <a href="tel:48667264375" className={"link-footer"}><img src={Phone} className={"ic-phone"} width={24} height={24} alt="Phone" /><p>573 462 766 84+</p></a>
-              <a href="mailto:magdalena.chuchmacz@wp.pl" className="link-footer"><img src={Message} className={"ic-message"} width={24} height={24} alt="Message" /><p>lp.pw@zcamhcuhc.aneladgam</p></a>
+              <a href="/" className={"link-footer"}><img src={LogoBlack} className={"ic-logo-black"} width={24} height={15} alt="Logo" /><p>{firstNameReverse} {lastNameReverse}</p></a>
+              <a href="tel:48667264375" className={"link-footer"}><img src={Phone} className={"ic-phone"} width={24} height={24} alt="Phone" /><p>{mobileNumberReverse}</p></a>
+              <a href="mailto:magdalena.chuchmacz@wp.pl" className="link-footer"><img src={Message} className={"ic-message"} width={24} height={24} alt="Message" /><p>{emailReverse}</p></a>
             </Col>
           </Row>
         </Container>
